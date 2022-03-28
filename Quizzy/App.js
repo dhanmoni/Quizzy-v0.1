1285daca199241dcd0b644373341ea0a04ee0c49
Dhanmoni Nath <dhanmoninath989@gmail.com>
2022-03-28 20:19:42 +0530

    redux persist added

--- Quizzy/App.js
+++ Quizzy/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { store } from './src/redux/store';
+import { store, persistor } from './src/redux/store';
+import { PersistGate } from 'redux-persist/integration/react'
+
 
 import Layout from './src/layout/Layout'
 const App = () => {
   return (
+    <PersistGate persistor={persistor}>
     <Provider store={store}>
       <Layout/>
     </Provider>
+    </PersistGate>
   );
 };
 
